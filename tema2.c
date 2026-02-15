47b670e1d1fb7495bf80abbeed18a23a3e6d8a6a
Rennanrta <rennan_torres@hotmail.com>
2026-02-15 02:12:55 +0000

    Desafio mestre

--- tema2.c
+++ tema2.c
@@ -2,10 +2,10 @@
 
 int main(){
 
-    int populacao1, populacao2, npt1, npt2;
-    char estado1[50], codigo1[50], estado2[50], codigo2[50];
-    char cidade1[50], cidade2[50];
-    float area1, area2, pib1, pib2;
+    int npt1, npt2;
+    long int populacao1, populacao2;
+    char estado1[50], codigo1[50], estado2[50], codigo2[50], cidade1[50], cidade2[50];
+    float area1, area2, pib1, pib2, dencidade1, dencidade2, pibcapita1, pibcapita2;
 
 // Declaração de variaveis (obs) npt - Numero de Pontos Turisticos
 // cada variavel tem 1 e 2 para armazenar os dados das duas cartas.
@@ -76,6 +76,47 @@ printf ("Digite o Numero de Pontos Turisticos da Carta 2:\n");
  
  //Gravando os dados da segunda carta
 
+dencidade1 = populacao1 / area1;
+pibcapita1 = (pib1*1000000000) / populacao1;
+
+//caluculando a dencidade populacional e PIB capita carta 1
+
+dencidade2 = populacao2 / area2;
+pibcapita2 = (pib2*1000000000) / populacao2;
+
+//caluculando a dencidade populacional e PIB capita carta 2
+
+int resulpopulacao, resularea, resulpib, resulnpt, resuldencidade, resulpibcapita, resulsuper;
+double inversaodenc1, inversaodenc2, super1, super2;
+
+// declaração de variaveis resuldado e inversao da dencidade 
+
+inversaodenc1 = area1 / populacao1;
+
+// inversão da dencidade carta  1
+
+inversaodenc2 = area2 / populacao2;
+
+// inversão da dencidade carta  2
+
+super1 = populacao1 + area1 + pib1 + npt1 + pibcapita1 + inversaodenc1;
+
+// soma das variaveis para o Super poder da carta 1
+
+super2 = populacao2 + area2 + pib2 + npt2 + pibcapita2 + inversaodenc2;
+
+// soma das variaveis para o Super poder da carta 2
+
+resulpopulacao = populacao1>populacao2;
+resularea = area1>area2;
+resulpib = pib1>pib2;
+resulnpt = npt1>npt2;
+resuldencidade = dencidade1<dencidade2;
+resulpibcapita = pibcapita1>pibcapita2;
+resulsuper = super1>super2;
+
+// variaveis de comparação das duas cartas
+
  printf(" Carta 1:\n");
  printf("\n");
  
@@ -86,6 +127,8 @@ printf ("Digite o Numero de Pontos Turisticos da Carta 2:\n");
  printf(" Aria: %f KM\n", area1);
  printf(" PIB: %f  Bilhões de Reais\n", pib1);
  printf(" Numeros de Pontos Turisticos: %d \n", npt1);
+ printf(" Densidade Populacional: %f Hab/Km \n", dencidade1);
+ printf(" PIB per Capita: %f Reais \n", pibcapita1);
  printf("\n");
 
  // Imprimindo dados na tela para o usuario da primeira carta
@@ -97,12 +140,27 @@ printf ("Digite o Numero de Pontos Turisticos da Carta 2:\n");
  printf(" Código: %s \n", codigo2);
  printf(" Nome da Cidade: %s \n", cidade2);
  printf(" População: %d \n", populacao2);
- printf(" Aria: %f KM\n", area2);
- printf(" PIB: %f Bilhões de Reais\n", pib2);
+ printf(" Aria: %.2f KM\n", area2);
+ printf(" PIB: %2.f Bilhões de Reais\n", pib2);
  printf(" Numeros de Pontos Turisticos: %d \n", npt2);
+ printf(" Densidade Populacional: %.2f Hab/Km \n", dencidade2);
+ printf(" PIB per Capita: %.2f Reais \n", pibcapita2);
  printf("\n");
 
 // Imprimindo dados na tela para o usuario da sugunda carta
+
+printf ("Resultado da comparação da cartas\n");
+printf ("\n");
+printf(" População: Carta 1 venceu ( %d ) \n", resulpopulacao);
+printf(" População: Carta 1 venceu ( %d ) \n", resularea);
+printf(" População: Carta 1 venceu ( %d ) \n", resulpib);
+printf(" População: Carta 1 venceu ( %d ) \n", resulnpt);
+printf(" População: Carta 2 venceu ( %d ) \n", resuldencidade);
+printf(" População: Carta 1 venceu ( %d ) \n", resulpibcapita);
+printf(" População: Carta 1 venceu ( %d ) \n", resulsuper);
+printf ("\n");
+
+// imprimir ao usuario as comparaçoes da carta
 
 return 0;
 }
